b7c6cfd70bbed30fd55f166e587194fd45ae84f6
ZEONe <julia3471@gmail.com>
2024-05-09 10:36:22 +0900

    헤더 완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,14 @@
 export default function menu() {
-  console.log(menu);
+  const headerToggle = document.getElementById("headerToggle");
+  const headerNav = document.querySelector(".header__nav");
+
+  if (headerToggle) {
+    headerToggle.addEventListener("click", () => {
+      headerNav.classList.toggle("show");
+      headerToggle.getAttribute("aria-expanded") === "true"
+        ? headerToggle.setAttribute("aria-expanded", "false")
+        : headerToggle.setAttribute("aria-expanded", "true");
+    });
+  }
+  https: console.log(menu);
 }
